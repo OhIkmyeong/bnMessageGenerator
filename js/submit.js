35f7a35f9a25c62dec32bb98fbfff2caa0bfdfa1
Oh JuYeon <dpffpself@naver.com>
2022-01-11 17:01:29 +0900

    20220111 조금수정

--- js/submit.js
+++ js/submit.js
@@ -3,7 +3,7 @@ import { make_end_string } from "./date.js";
 export function get_data(e){
     const result = {
         name : e.target[0].value,
-        path : [],
+        path : undefined,
         recommit : e.target["recommit"].checked,
     }
     return result;
@@ -12,17 +12,21 @@ export function get_data(e){
 export async function on_submit(data){
     const $result = document.getElementById('result');
     let result = '';
-    //업무명
-    const busiName = data.name ? `[${data.name}]<br>` : '';
-    result += busiName;
 
     //경로 싹 다 추가
     data.path = await inspect_paths();
     const busiPath = add_path_string_all(data);
-    result += busiPath;
+    if(!busiPath){return;}
+
+    //업무명
+    const busiName = data.name ? `[${data.name}]<br>` : '';
 
     //마지막 말 추가하기
     const endString = make_end_string(data);
+
+    //조합
+    result += busiName;
+    result += busiPath;
     result += endString;
 
     //표시
@@ -32,14 +36,17 @@ export async function on_submit(data){
 /* path 검증 */
 async function inspect_paths(){
     const all_path = document.querySelectorAll('.iptPath');
+    const result = [];
     for(let i=all_path.length - 1; i>=0; i--){
         const trimmed = all_path[i].value.trim(); 
         if(!trimmed){
             if(all_path.length!=1){all_path[i].parentElement.remove();}
+        }else{
+            all_path[i].value = trimmed;
+            result.push(all_path[i]);
         }//if
-        all_path[i].value = trimmed;
     }//for
-    return all_path;
+    return result;
 }//inspect_paths
 
 /* 모든 path 더하기 */
@@ -49,7 +56,7 @@ function add_path_string_all(data){
     path.forEach((ipt,idx)=>{
         if(ipt.value){
             result += `(${idx}) ${ipt.value}<br>`;
-        }//if
+        }
     });
     return result;
 }//add_path_string_all
